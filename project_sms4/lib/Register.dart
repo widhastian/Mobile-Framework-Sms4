2cf810189ae8ceed3d461a1fb9de5775473447b0
nurillahilwafi <nurilwafi17@gmail.com>
2022-05-29 15:38:08 +0700

    revisi register.dart

--- project_sms4/lib/Register.dart
+++ project_sms4/lib/Register.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:from_css_color/from_css_color.dart';
+import 'package:project_sms4/Home.dart';
+import 'package:project_sms4/Login.dart';
+import 'package:project_sms4/SpashScreen.dart';
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -10,8 +13,404 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  late bool _passwordVisible;
+  final _formKey = GlobalKey<FormState>();
+  @override
+  void initState() {
+    super.initState();
+    _passwordVisible = false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Container(
+                    alignment: Alignment.topLeft,
+                    color: Colors.transparent,
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_back_ios_new_rounded),
+                      iconSize: 25,
+                      color: Colors.black,
+                      splashColor: fromCssColor('#6AA83F'),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SplashScreenWidget()));
+                      },
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(-0.35, 0),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                    child: Text(
+                      'Selamat Datang di CFarmer!',
+                      textAlign: TextAlign.start,
+                      style: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 21,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: new InputDecoration(
+                      hintText: "contoh: widha astiani",
+                      hintStyle: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 12,
+                      ),
+                      labelText: "masukkan nama lengkap",
+                      labelStyle: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 14,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: fromCssColor('#6AA83F'),
+                          width: 1,
+                        ),
+                        borderRadius: new BorderRadius.circular(8.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: fromCssColor('#6AA83F'),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(
+                        Icons.account_circle,
+                        color: fromCssColor('#6AA83F'),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'nama tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: new InputDecoration(
+                      hintText: "contoh: o85xxxxxxxxx",
+                      hintStyle: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 12,
+                      ),
+                      labelText: "masukkan nomor hp",
+                      labelStyle: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 14,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: fromCssColor('#6AA83F'),
+                          width: 1,
+                        ),
+                        borderRadius: new BorderRadius.circular(8.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: fromCssColor('#6AA83F'),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(
+                        Icons.add_call,
+                        color: fromCssColor('#6AA83F'),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'nomor hp tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: new InputDecoration(
+                      hintText: "pilih kategori",
+                      hintStyle: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 12,
+                      ),
+                      labelText: "Kategori",
+                      labelStyle: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 14,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: fromCssColor('#6AA83F'),
+                          width: 1,
+                        ),
+                        borderRadius: new BorderRadius.circular(8.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: fromCssColor('#6AA83F'),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(
+                        Icons.list_rounded,
+                        color: fromCssColor('#6AA83F'),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'kategori tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    obscureText: !_passwordVisible,
+                    decoration: new InputDecoration(
+                      suffixIcon: IconButton(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 15, 0),
+                        icon: Icon(
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: fromCssColor('#6AA83F'),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                      ),
+                      hintText: "Masukkan password anda",
+                      hintStyle: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 12,
+                      ),
+                      labelText: "Password",
+                      labelStyle: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 14,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: fromCssColor('#6AA83F'),
+                          width: 1,
+                        ),
+                        borderRadius: new BorderRadius.circular(8.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: fromCssColor('#6AA83F'),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: fromCssColor('#6AA83F'),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Password tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    obscureText: !_passwordVisible,
+                    decoration: new InputDecoration(
+                      suffixIcon: IconButton(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 15, 0),
+                        icon: Icon(
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: fromCssColor('#6AA83F'),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                      ),
+                      hintText: "Masukkan password anda",
+                      hintStyle: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 12,
+                      ),
+                      labelText: "Konfirmasi Password",
+                      labelStyle: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 14,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: fromCssColor('#6AA83F'),
+                          width: 1,
+                        ),
+                        borderRadius: new BorderRadius.circular(8.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: fromCssColor('#6AA83F'),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: fromCssColor('#6AA83F'),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Password tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 340, 0, 0),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Home()));
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: fromCssColor('#6AA83F'),
+                      onPrimary: Colors.white,
+                      minimumSize: Size(200, 50),
+                      side: BorderSide(
+                        color: Colors.transparent,
+                        width: 1,
+                      ),
+                      textStyle: GoogleFonts.getFont(
+                        'Poppins',
+                        // color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(8.0),
+                      ),
+                      elevation: 3,
+                    ),
+                    child: Text("Daftar"),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 6),
+                        child: Text(
+                          'Sudah punya akun?',
+                          style: GoogleFonts.getFont(
+                            'Poppins',
+                            color: Colors.black45,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(1, 0, 0, 0),
+                        child: TextButton(
+                          child: Text(
+                            'Masuk disini!',
+                            style: GoogleFonts.getFont(
+                              'Poppins',
+                              color: fromCssColor('#6AA83F'),
+                              fontWeight: FontWeight.w700,
+                              fontSize: 14,
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Login()));
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
