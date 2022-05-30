8b4aedd869fe3636088ebf032d5ab8b1794f673e
DYAH SAFITRI RESWANDARI <dyahsafitrii20@gmail.com>
2022-05-30 10:54:00 +0700

    Menambahkan file setting.dart

---
+++ project_sms4/lib/Setting.dart
@@ -0,0 +1,96 @@
+import 'package:project_sms4/SpashScreen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:from_css_color/from_css_color.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SettingPage extends StatefulWidget {
+
+  @override
+  _SettingPageState createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  void initState() {
+    super.initState();
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Container(
+                    alignment: Alignment.topLeft,
+                    color: Colors.transparent,
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_back_ios_new_rounded),
+                      iconSize: 25,
+                      color: Colors.black,
+                      splashColor: fromCssColor('#6AA83F'),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SplashScreenWidget()
+                                // belum mengubah navigator button back
+                            )
+                        ); 
+                      },
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0, 0),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                    child: Text(
+                      'Setting',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.getFont(
+                        'Poppins',
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 26,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(1, 0, 0, 0),
+                        child: TextButton(
+                          child: Text(
+                            'Simpan',
+                            style: GoogleFonts.getFont(
+                              'Poppins',
+                              color: fromCssColor('#6AA83F'),
+                              fontWeight: FontWeight.w700,
+                              fontSize: 14,
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SplashScreenWidget()));
+                          },
+                        ),
+                      ),
+              ]
+            )
+          )
+        )
+      )
+    );
+  }
+}
