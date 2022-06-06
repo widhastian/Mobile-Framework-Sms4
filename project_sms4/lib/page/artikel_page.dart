af38e241438901821563375b9e6b5f203df9456c
DYAH SAFITRI RESWANDARI <dyahsafitrii20@gmail.com>
2022-06-06 12:14:12 +0700

    edit searching artikel_page

--- project_sms4/lib/page/artikel_page.dart
+++ project_sms4/lib/page/artikel_page.dart
@@ -34,7 +34,7 @@ class _ArtikelState extends State<Artikel> {
             Align(
               alignment: AlignmentDirectional(0, 0),
               child: Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 25),
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
                   child: Text(
                     'Artikel',
                     textAlign: TextAlign.center,
@@ -48,7 +48,70 @@ class _ArtikelState extends State<Artikel> {
               ),
             ),
             // PENCARIAN
-            
+            Align(
+              child: GestureDetector(
+                onTap: () => FocusScope.of(context).unfocus(),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(10, 20, 10, 15),
+                      child: TextFormField(
+                        controller: textController,
+                        onChanged: (_) => EasyDebounce.debounce(
+                          'textController',
+                          Duration(milliseconds: 1000),
+                          () => setState(() {}),
+                        ),
+                        autofocus: true,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          labelText: 'Cari',
+                          hintText: 'Cari artikel disini',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF6AA83F),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF6AA83F),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search_rounded,
+                            color: Color(0xFF6AA83F),
+                            size: 20,
+                          ),
+                          suffixIcon: textController.text.isNotEmpty
+                              ? InkWell(
+                                  onTap: () => setState(
+                                    () => textController.clear(),
+                                  ),
+                                  child: Icon(
+                                    Icons.clear,
+                                    color: Color(0xFF757575),
+                                    size: 22,
+                                  ),
+                                )
+                              : null,
+                        ),
+                        style: GoogleFonts.getFont(
+                          'Poppins',
+                          color: Colors.black,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             Card(
               child: Container(
                 height: 270, //ukuran cardnya
@@ -174,83 +237,6 @@ class _ArtikelState extends State<Artikel> {
         ),
       ),],
     ),
-      // body: SafeArea(
-      //   child: GestureDetector(
-      //     onTap: () => FocusScope.of(context).unfocus(),
-      //     child: Column(
-      //       mainAxisSize: MainAxisSize.max,
-      //       crossAxisAlignment: CrossAxisAlignment.stretch,
-      //       children: [
-      //         Padding(
-      //           padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
-      //           child: Text(
-      //             'Artikel',
-      //             textAlign: TextAlign.center,
-      //             style: GoogleFonts.getFont(
-      //               'Poppins',
-      //               color: Colors.black,
-      //               fontWeight: FontWeight.w600,
-      //               fontSize: 24,
-      //             ),
-      //           ),
-      //         ),
-      //         Padding(
-      //           padding: EdgeInsetsDirectional.fromSTEB(10, 20, 10, 0),
-      //           child: TextFormField(
-      //             controller: textController,
-      //             onChanged: (_) => EasyDebounce.debounce(
-      //               'textController',
-      //               Duration(milliseconds: 1000),
-      //               () => setState(() {}),
-      //             ),
-      //             autofocus: true,
-      //             obscureText: false,
-      //             decoration: InputDecoration(
-      //               labelText: 'Cari',
-      //               hintText: 'Cari artikel disini',
-      //               enabledBorder: OutlineInputBorder(
-      //                 borderSide: BorderSide(
-      //                   color: Color(0xFF6AA83F),
-      //                   width: 1,
-      //                 ),
-      //                 borderRadius: BorderRadius.circular(10),
-      //               ),
-      //               focusedBorder: OutlineInputBorder(
-      //                 borderSide: BorderSide(
-      //                   color: Color(0xFF6AA83F),
-      //                   width: 1,
-      //                 ),
-      //                 borderRadius: BorderRadius.circular(10),
-      //               ),
-      //               prefixIcon: Icon(
-      //                 Icons.search_rounded,
-      //                 color: Color(0xFF6AA83F),
-      //                 size: 20,
-      //               ),
-      //               suffixIcon: textController.text.isNotEmpty
-      //                   ? InkWell(
-      //                       onTap: () => setState(
-      //                         () => textController.clear(),
-      //                       ),
-      //                       child: Icon(
-      //                         Icons.clear,
-      //                         color: Color(0xFF757575),
-      //                         size: 22,
-      //                       ),
-      //                     )
-      //                   : null,
-      //             ),
-      //             style: GoogleFonts.getFont(
-      //               'Poppins',
-      //               color: Colors.black,
-      //               fontSize: 12,
-      //             ),
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // ),
     );
   }
 }
