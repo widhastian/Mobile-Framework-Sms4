8fc50f20f154e126266fd414db8bdd12668fa2ec
nurillahilwafi <nurilwafi17@gmail.com>
2022-06-07 09:51:01 +0700

    create api

---
+++ project_sms4/lib/utils/api.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Network{
+  final String _url = 'http://localhost:8000/api/v1';
+  //masih bingung link yang dipakai yang mana..
+  //link file api pada laravel
+  //if you are using android studio emulator, change localhost to 10.0.2.2
+  var token;
+
+  _getToken() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance(); 
+    // token = jsonDecode(localStorage.getString('token'))['token'];
+    token = jsonDecode(localStorage.getString('token') ?? '')['token'];
+    print('Token $token');
+  }
+  
+  // localStorage.setString('token', json.encode(['token']));
+  authData(data, apiUrl) async {
+    var fullUrl = _url + apiUrl;
+    return await http.post(
+        Uri.parse(fullUrl),
+        body: jsonEncode(data),
+        headers: _setHeaders()
+    );
+  }
+
+  getData(apiUrl) async {
+    var fullUrl = _url + apiUrl;
+    await _getToken();
+    return await http.get(
+        Uri.parse(fullUrl),
+        headers: _setHeaders()
+    );
+  }
+
+  _setHeaders() => {
+    'Content-type' : 'application/json',
+    'Accept' : 'application/json',
+    'Authorization' : 'Bearer $token'
+  };
+
+}
